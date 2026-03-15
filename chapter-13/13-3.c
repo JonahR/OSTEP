667cf6cf8675384287d37fa0e3093521a54c74b9
Jonah Rueb <ruebjonah@gmail.com>
2026-03-15 18:18:14 -0400

    Chapter 13 partial solutions

---
+++ chapter-13/13-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("usage: %s <size in MB>\n", argv[0]);
+    }
+
+    int bytes = 100000 * atoi(argv[1]);
+    char *array = (char *) malloc(sizeof(char) * bytes);
+    while (1) {
+        for (int i = 0; i < bytes; i++) {
+            array[i] += array[i];
+        }
+    }
+}
